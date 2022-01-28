2e74b2fa1ad6606bd517fb04206978a6be907416
LevitatingBusinessMan <public@reinfernhout.xyz>
2022-01-28 20:13:57 +0100

    something is working

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ use orca::Sort as RedditSort;
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-struct RedditFS;
+struct RedditFS {
+    reddit: orca::App
+}
+
 static README_TEXT: &'static str = "Reddit filesystem\n";
 
 macro_rules! debug {
@@ -60,23 +63,37 @@ lazy_static! {
         blksize: 512,
     };
 
+    static ref SUBREDDIT_DIR_ATTR: FileAttr = FileAttr {
+        ino: 3,
+        size: 0,
+        blocks: 0,
+        atime: SystemTime::now(),
+        mtime: SystemTime::now(),
+        ctime: SystemTime::now(),
+        crtime: SystemTime::now(),
+        kind: FileType::Directory,
+        perm: 0o755,
+        nlink: 2,
+        uid: 501,
+        gid: 20,
+        rdev: 0,
+        flags: 0,
+        blksize: 512,
+    };
+
     static ref files: Mutex<HashMap<String, File>> = {let mut m = HashMap::new();Mutex::new(m)};
 }
 
-static mut last_inode: u64 = 2;
+static mut last_inode: u64 = 3;
 
 struct File {
     content: String,
     attr: FileAttr,
 }
 
-
-thread_local! {
-    static  REDDIT_CLIENT: orca::App = RedditCLient::new("reddit-fs", env!("CARGO_PKG_VERSION"), "LevitatingBusinessMan").unwrap();
-}
-
 const TTL: Duration = Duration::from_secs(1);
 
+//https://libfuse.github.io/doxygen/structfuse__operations.html
 impl Filesystem for RedditFS {
     fn lookup(&mut self, req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
         let name = name.to_str().unwrap();
@@ -87,20 +104,20 @@ impl Filesystem for RedditFS {
                 reply.entry(&TTL, &README_FILE_ATTR, 0);
                 return;
             } else if !name.contains(".") {
-                REDDIT_CLIENT.with(|reddit| {
-                    let fetch_result = reddit.get_posts(name, RedditSort::Hot);
-                    match fetch_result {
-                        Ok(res) => {
-                            let posts = res.get("data").unwrap().get("children").unwrap();
-                            /* for post in posts.as_array().unwrap() {
-                                reply.entry(&TTL, &create_post_file(post), 0);
-                            } */ // This code should be moved to readdir instead. Lookup should return the subreddit as a directory.
-                        },
-                        Err(err) => {
-                            eprint!("{}", "Request failed");
-                        }  
+                reply.entry(&TTL, &SUBREDDIT_DIR_ATTR, 0);
+                return;
+                let fetch_result = self.reddit.get_posts(name, RedditSort::Hot);
+                match fetch_result {
+                    Ok(res) => {
+                        let posts = res.get("data").unwrap().get("children").unwrap();
+                        /* for post in posts.as_array().unwrap() {
+                            reply.entry(&TTL, &create_post_file(post), 0);
+                        } */ // This code should be moved to readdir instead. Lookup should return the subreddit as a directory.
+                    },
+                    Err(err) => {
+                        eprint!("{}", "Request failed");
                     }
-                });
+                }
                 return;
             }
         }
@@ -116,18 +133,43 @@ impl Filesystem for RedditFS {
             _ => reply.error(ENOENT),
         }
     }
+
     fn readdir(&mut self, _req: &Request<'_>, ino: u64, _fh: u64, offset: i64, mut reply: fuser::ReplyDirectory) {
         debug!("readdir: ino {:?} offset {:?}", ino, offset);
-        if ino != 1 {
-            reply.error(ENOENT);
-            return;
-        }
-
-        let entries = vec![
+        
+        let mut entries = vec![
             (1, FileType::Directory, "."),
             (1, FileType::Directory, ".."),
-            (2, FileType::RegularFile, "README.txt"),
         ];
+
+        match ino {
+            1 => entries.push((2, FileType::RegularFile, "README.txt")),
+            3 => {
+                let fetch_result = self.reddit.get_posts("linux", RedditSort::Hot);
+                match fetch_result {
+                    Ok(res) => {
+                        let posts = res.get("data").unwrap().get("children").unwrap();
+                        let mut last_inode2 = 3;
+                        let mut i: i64 = 0;
+                        for post in posts.as_array().unwrap() {
+                            let id = post.get("data").unwrap().get("id").unwrap().as_str().unwrap().to_owned();
+                            reply.add(last_inode2, i + 1, FileType::RegularFile, &id);
+                            last_inode2 += 1;
+                            i += 1;
+                        }
+                        reply.ok();
+                        return;
+                    },
+                    Err(err) => {
+                        eprint!("{}", "Reddit request failed");
+                    }
+                }
+            },
+            _ => {
+                reply.error(ENOENT);
+                return;
+            }
+        }
 
         for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
             // i + 1 means the index of the next entry
@@ -135,6 +177,7 @@ impl Filesystem for RedditFS {
                 break;
             }
         }
+
         reply.ok();
     }
 
@@ -187,6 +230,9 @@ fn create_post_file(post: &serde_json::Value) -> FileAttr {
 }
 
 fn main() -> Result<()> {
-    fuser::mount2(RedditFS, &Path::new("/home/rein/reddit"), &[])?;
+    let fs = RedditFS {
+        reddit: RedditCLient::new("reddit-fs", env!("CARGO_PKG_VERSION"), "LevitatingBusinessMan").unwrap()
+    };
+    fuser::mount2(fs, &Path::new("/home/rein/reddit"), &[])?;
     Ok(())
 }
